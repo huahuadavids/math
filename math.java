0e488fbe438d230fae8debe13e0188859a3fc2e2
yuhua <fangao.zhang@tuya.com>
2021-10-20 17:35:19 +0800

    feat: add 对数组分大小两部分的理解

--- math.java
+++ math.java
@@ -1,17 +1,21 @@
 class Solution {
-
-    public int getSum(int a, int b) {
-        int sum = a ^ b;
-        int temp = (a & b) << 1;
-        if (temp != 0) {
-            return getSum(sum, temp);
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
         }
-        return sum;
+
+        return candidate;
     }
 
     public static void main(String args[]) {
         Solution s = new Solution();
-        int arr[] = { 1, 2, 3 };
+        int arr[] = { 1,1,1,2,2,2,3,3 };
+        System.out.println(s.majorityElement(arr));
     }
 
 }
